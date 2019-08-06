f98f124b259d5a5eef8bd36c702ba11a15061623
Azerxim <mallet.maxime@hotmail.fr>
2019-08-06 14:19:27 +0200

    tabulation

--- DB.py
+++ DB.py
@@ -8,68 +8,68 @@ db = TinyDB(DB_NOM)
 inv = dict()
 
 def dbExist():
-    """
-    Retourne True ou False en fonction de si la db existe.
-    """
-    try:
-        with open(DB_NOM): pass
-    except IOError:
-        return False
-    return True
+	"""
+	Retourne True ou False en fonction de si la db existe.
+	"""
+	try:
+		with open(DB_NOM): pass
+	except IOError:
+		return False
+	return True
 
 def newPlayer(ID):
-    """
-    Permet d'ajouter un nouveau joueur à la base de donnée en fonction de son ID.
+	"""
+	Permet d'ajouter un nouveau joueur à la base de donnée en fonction de son ID.
 
-    ID: int de l'ID du joueur
-    """
+	ID: int de l'ID du joueur
+	"""
 
-    if db.search(Query().ID == ID) == []:
-        #Init du joueur avec les champs de base
-        #########################MODIFIER ICI SI NVX CHAMPS#####################
-        db.insert({'ID': ID, 'arrival': str(dt.datetime.now()),'com_time': 0,'gems':0, 'inventory':inv, 'nbMsg': 0, 'lvl': 0})
-        ########################################################################
-        return ("Le joueur a été ajouté !")
-    else:
-        return ("Le joueur existe déjà")
+	if db.search(Query().ID == ID) == []:
+		#Init du joueur avec les champs de base
+		#########################MODIFIER ICI SI NVX CHAMPS#####################
+		db.insert({'ID': ID, 'arrival': str(dt.datetime.now()),'com_time': 0,'gems':0, 'inventory':inv, 'nbMsg': 0, 'lvl': 0})
+		########################################################################
+		return ("Le joueur a été ajouté !")
+	else:
+		return ("Le joueur existe déjà")
 
 
 def updateField(ID, fieldName, fieldValue):
-    """
-    Permet de mettre à jour la valeur fieldName par la fieldValue.
+	"""
+	Permet de mettre à jour la valeur fieldName par la fieldValue.
 
-    ID: int de l'ID du joueur.
-    fieldName: string du nom du champ à changer
-    fieldValue: string qui va remplacer l'ancienne valeur
-    """
-    if db.search(getattr(Query(),fieldName)) == []:
-        print("Le champ n'existe pas")
-        return "201"
-    else:
-        db.update({fieldName: fieldValue}, Query().ID == ID)
-        print("Le champ a été mis à jour")
-        return "200"
+	ID: int de l'ID du joueur.
+	fieldName: string du nom du champ à changer
+	fieldValue: string qui va remplacer l'ancienne valeur
+	"""
+	if db.search(getattr(Query(),fieldName)) == []:
+		print("Le champ n'existe pas")
+		return "201"
+	else:
+		db.update({fieldName: fieldValue}, Query().ID == ID)
+		print("Le champ a été mis à jour")
+		return "200"
 
 
 def valueAt(ID, fieldName):
-    """
-    Permet de récupérer la valeur contenue dans le champ fieldName de ID
+	"""
+	Permet de récupérer la valeur contenue dans le champ fieldName de ID
 
-    ID: int de l'ID du joueur
-    fieldName: string du nom du champ à chercher
-    """
-    return db.search(Query().ID == ID)[0][fieldName]
+	ID: int de l'ID du joueur
+	fieldName: string du nom du champ à chercher
+	"""
+	return db.search(Query().ID == ID)[0][fieldName]
 
 async def count(message):
-    l=len(db)
-    if l == 0:
-        await message.channel.send('Aucun utilisaeur enregistrer dans la BDD')
-    else:
-        await message.channel.send('{l} utilisateur inscrit')
-    return l
+	l=len(db)
+	if l == 0:
+		await message.channel.send('Aucun utilisaeur enregistrer dans la BDD')
+	else:
+		await message.channel.send('{l} utilisateur inscrit')
+	return l
 
 def updateComTime(ID):
-    """
-    Met à jour la date du dernier appel à une fonction
-    """
-    updateField(ID, "com_time", t.time())
+	"""
+	Met à jour la date du dernier appel à une fonction
+	"""
+	updateField(ID, "com_time", t.time())
--- roles.py
+++ roles.py
@@ -5,62 +5,62 @@ client = discord.Client()
 
 @client.event  # event decorator/wrapper.
 async def on_ready():  # méthode attendue par le client. Cela fonctionne une fois connecté
-    print('| Roles Module | >> Connecté !')  # notification de login.
+	print('| Roles Module | >> Connecté !')  # notification de login.
 
 async def autorole(member):
-    role = discord.utils.get(member.guild.roles, name="Joueurs")
-    await member.add_roles(role)
+	role = discord.utils.get(member.guild.roles, name="Joueurs")
+	await member.add_roles(role)
 
 class Roles(commands.Cog):
 
-    def __init__(self,ctx):
-        return(None)
+	def __init__(self,ctx):
+		return(None)
 
-    @commands.command(pass_context=True)
-    async def creategame(self, ctx, game, categorie):
-        """
-        Permet de créer un nouveau role pour un jeu et d'ajouter ce jeu dans un grand salon
-        Paramètres:
-        - game: Nom du jeu/role
-        - categorie: Nom du grand salon (combat/societe/tirs/voiture/rpg/sandbox/strategie/divers)
-        """
-        guild = ctx.guild
-        member = ctx.author
-        rolesearch = discord.utils.get(member.guild.roles, name=game)
-        if rolesearch == None:
-            await guild.create_role(name=game)
-            msg = "Le jeu '"+game+"' a été créé"
+	@commands.command(pass_context=True)
+	async def creategame(self, ctx, game, categorie):
+		"""
+		Permet de créer un nouveau role pour un jeu et d'ajouter ce jeu dans un grand salon
+		Paramètres:
+		- game: Nom du jeu/role
+		- categorie: Nom du grand salon (combat/societe/tirs/voiture/rpg/sandbox/strategie/divers)
+		"""
+		guild = ctx.guild
+		member = ctx.author
+		rolesearch = discord.utils.get(member.guild.roles, name=game)
+		if rolesearch == None:
+			await guild.create_role(name=game)
+			msg = "Le jeu '"+game+"' a été créé"
 
-            if categorie != None:
-                rolesearch = discord.utils.get(member.guild.roles, name=game)
-                categorie = categorie.lower()
+			if categorie != None:
+				rolesearch = discord.utils.get(member.guild.roles, name=game)
+				categorie = categorie.lower()
 
-                if categorie == "combat":
-                    channeladd = guild.get_channel(589944955800256515)
-                elif categorie == "societe":
-                    channeladd = guild.get_channel(589945591203889152)
-                elif categorie == "tirs":
-                    channeladd = guild.get_channel(589946246437797888)
-                elif categorie == "voiture":
-                    channeladd = guild.get_channel(589946276540448821)
-                elif categorie == "rpg":
-                    channeladd = guild.get_channel(589946305917222916)
-                elif categorie == "sandbox":
-                    channeladd = guild.get_channel(589946380416581632)
-                elif categorie == "strategie":
-                    channeladd = guild.get_channel(589953946639007764)
-                elif categorie == "divers":
-                    channeladd = guild.get_channel(590664052318142474)
-                else:
-                    channeladd = guild.get_channel(589942497678196764)# 590664052318142474
-                await channeladd.set_permissions(rolesearch, overwrite=discord.PermissionOverwrite(read_messages=True))
-                msg = "Ajout d'un nouveau jeu dans la catégorie {}: {}".format(channeladd.mention, rolesearch.mention)
-                channel = guild.get_channel(417449076775321600)
-                return (channel.send(msg))
-            else:
-                await ctx.channel.send(msg)
-        else:
-            await ctx.channel.send("Le jeu "+game+" existe déjà")
+				if categorie == "combat":
+					channeladd = guild.get_channel(589944955800256515)
+				elif categorie == "societe":
+					channeladd = guild.get_channel(589945591203889152)
+				elif categorie == "tirs":
+					channeladd = guild.get_channel(589946246437797888)
+				elif categorie == "voiture":
+					channeladd = guild.get_channel(589946276540448821)
+				elif categorie == "rpg":
+					channeladd = guild.get_channel(589946305917222916)
+				elif categorie == "sandbox":
+					channeladd = guild.get_channel(589946380416581632)
+				elif categorie == "strategie":
+					channeladd = guild.get_channel(589953946639007764)
+				elif categorie == "divers":
+					channeladd = guild.get_channel(590664052318142474)
+				else:
+					channeladd = guild.get_channel(589942497678196764)# 590664052318142474
+				await channeladd.set_permissions(rolesearch, overwrite=discord.PermissionOverwrite(read_messages=True))
+				msg = "Ajout d'un nouveau jeu dans la catégorie {}: {}".format(channeladd.mention, rolesearch.mention)
+				channel = guild.get_channel(417449076775321600)
+				return (channel.send(msg))
+			else:
+				await ctx.channel.send(msg)
+		else:
+			await ctx.channel.send("Le jeu "+game+" existe déjà")
 
 def setup(bot):
 	bot.add_cog(Roles(bot))
