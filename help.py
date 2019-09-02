eae72f6e5d41b221f9c05368551624751ff086d8
Azerxim <mallet.maxime@hotmail.fr>
2019-09-02 15:20:23 +0200

    amelioration de la commande help

--- help.py
+++ help.py
@@ -12,26 +12,37 @@ class Helpme(commands.Cog):
 		self.bot = bot
 
 	@commands.command(pass_context=True)
-	async def help(self, ctx):
+	async def help(self, ctx, nameElem = None):
 		"""Affiche ce message !"""
 		d_help = "Liste de toutes les fonctions utilisable avec le prefix {}".format(self.PREFIX)
 		msg = discord.Embed(title = "Fonction disponible",color= 12745742, description = d_help)
-		
+
 		COGS = open("fichier_txt/cogs.txt","r").read()
 		COGS = COGS.split('\n')
 		COGS.pop()
 		for COG in COGS:
-			cog = self.bot.get_cog(COG)
-			coms = cog.get_commands()
-			arg = ""
-			for com in coms :
-				arg += "•"+str(com.name)+" : "+str(com.help)+"\n"
-			if COG == "Helpme":
-				msg.add_field(name=COG, value=arg, inline=False)
+			if nameElem != None:
+				mCOG = COG.lower()
+				nameElem = nameElem.lower()
+				if mCOG == nameElem:
+					cog = self.bot.get_cog(COG)
+					coms = cog.get_commands()
+					arg = ""
+					for com in coms :
+						arg += "•"+str(com.name)+" : "+str(com.help)+"\n"
+					msg.add_field(name=COG, value=arg, inline=False)
+					await ctx.send(embed = msg, delete_after = 60)
 			else:
-				msg = discord.Embed(title = COG,color= 12745742, description = arg)
-
-			await ctx.send(embed = msg, delete_after = 60)
+				cog = self.bot.get_cog(COG)
+				coms = cog.get_commands()
+				arg = ""
+				for com in coms :
+					arg += "•"+str(com.name)+" : "+str(com.help)+"\n"
+				if COG == "Helpme":
+					msg.add_field(name=COG, value=arg, inline=False)
+				else:
+					msg = discord.Embed(title = COG,color= 12745742, description = arg)
+				await ctx.send(embed = msg, delete_after = 60)
 
 def setup(bot):
 	bot.add_cog(Helpme(bot))
