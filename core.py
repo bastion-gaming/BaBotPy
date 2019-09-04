eacdb17a04a1cf693eefa92ece158fea32f8002d
gnouf1 <jeuxforum26@gmail.com>
2019-09-05 00:48:30 +0200

    cgt message bienvenue

--- core.py
+++ core.py
@@ -93,9 +93,9 @@ async def on_member_join(member):
 		#c = data.cursor()
 		id = member.id
 		if DB.newPlayer(id) == "Le joueur a été ajouté !":
-			msg = ":black_small_square:Bienvenue {0} sur Bastion!:black_small_square: \n\n\nNous sommes ravis que tu aies rejoint notre communauté ! \nTu es attendu : \n\n:arrow_right: Sur #⌈:closed_book:⌋•règles \n:arrow_right: Sur #⌈:ledger:⌋•liste-salons\n\n=====================".format(member.mention)
+			msg = ":black_small_square:Bienvenue {0} sur Bastion!:black_small_square: \n\n\nNous sommes ravis que tu aies rejoint notre communauté ! \nTu es attendu : \n\n:arrow_right: Sur #417454223224209408 \n:arrow_right: Sur #545204163341058058\nAjoute aussi ton parrain avec `!parrain <Nom>`\n\n=====================".format(member.mention)
 		else:
-			msg = "=====================\nBon retour parmis nous ! {0}\n\n=====================".format(member.mention)
+			msg = "===================== Bon retour parmis nous ! {0} =====================".format(member.mention)
 		stat.countCo()
 		await channel.send(msg)
 
