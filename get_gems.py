4bc281927b82fe09d531dd6de9db2ddcd1a51648
Azerxim <mallet.maxime@hotmail.fr>
2019-09-23 15:35:31 +0200

    Taxe GetGems

--- get_gems.py
+++ get_gems.py
@@ -3,11 +3,16 @@ from discord.ext import commands, tasks
 from discord.ext.commands import Bot
 from discord.utils import get
 
+import DB
+
 # initialisation des variables.
-DEFAUT_PREFIX = "*"
+DEFAUT_PREFIX = "'"
+
+idGetGems = 620558080551157770
+idBASTION = 417445502641111051
 
 VERSION = open("fichier_txt/version.txt").read().replace("\n","")
-TOKEN = open("fichier_txt/token_getgems.txt", "r").read().replace("\n","")
+TOKEN = open("fichier_txt/token.txt", "r").read().replace("\n","")
 client = commands.Bot(command_prefix = "{0}".format(DEFAUT_PREFIX))
 NONE = open("fichier_txt/cogs.txt","w")
 NONE = open("fichier_txt/help.txt","w")
@@ -21,6 +26,19 @@ async def on_ready():
 	print('PREFIX = '+str(DEFAUT_PREFIX))
 	print('\nBastionBot '+VERSION)
 	print('------\n')
+
+
+@client.event
+async def on_member_remove(member):
+	ID = member.id
+	if member.guild.id != idBASTION:
+		gems = DB.valueAt(ID, "gems")
+		BotGems = DB.valueAt(idGetGems, "gems")
+		pourcentage = 0.02
+		transfert = gems * pourcentage
+		DB.updateField(idGetGems, "gems", BotGems + int(transfert))
+		DB.updateField(ID, "gems", gems - int(transfert))
+
 
 ####################### Commande help.py #######################
 
