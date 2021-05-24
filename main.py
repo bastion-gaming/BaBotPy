4c2f4f391650ed3b92a6c5a9e71f86ba355ddde2
Azerxim <mallet.maxime@hotmail.fr>
2021-05-24 23:28:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ client.load_extension('core.gestion')
 
 ##################### Commande images.py #####################
 
-# client.load_extension('multimedia.images')
+client.load_extension('multimedia.images')
 
 ###################### Commande parrain.py ########################
 
